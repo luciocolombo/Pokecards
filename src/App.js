6e0b93e4880d724f21fc62227540c3ebe6e7d776
luciocolombo <colombolucio@hotmail.com>
2021-06-15 15:49:18 -0300

    fixed height and minor visual issues

--- src/App.js
+++ src/App.js
@@ -1,66 +1,71 @@
-
-import './App.css';
-import React from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { render } from '@testing-library/react';
-
-
-
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Card, Button } from "react-bootstrap";
 
 export default class PersonList extends React.Component {
-  constructor(props){
-  super(props);
-  this.state = {
-    pokemons: [],
-    name:""
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons: [],
+      name: "",
+    };
   }
-}
-
 
   componentDidMount() {
     this.axxios();
   }
-  axxios(){
-    const numero=Math.round(Math.random()*893);
-      axios.get(`https://pokeapi.co/api/v2/pokemon/${numero}`)
-      .then(res => {
-        const pokemons = res.data.sprites;
-        let name=numero;
-        if(typeof(res.data.forms[0])!=="undefined"){
+  axxios() {
+    const numero = Math.round(Math.random() * 893);
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${numero}`).then((res) => {
+      const pokemons = res.data.sprites;
+      let name = numero;
+      if (typeof res.data.forms[0] !== "undefined") {
         this.setState({
-          name:res.data.forms[0].name,
-          pokemons:res.data.sprites});
-        }
-      })
-  } 
-  render(){
-  return(
-      <div class="App shadow">
-      <div class=" d-flex justify-content-center ">
-        <div class="card m-5 w-25 carta text-center shadow" >
-          <img src={this.state.pokemons.front_default} class="card-img-top-left imagenpokemon mt-3" alt="img"></img>
-          <div class="card-body border">
-            <h5 class="card-title">{this.state.name}</h5>
-            
-            <a class="d-block" href={`https://en.wikipedia.org/wiki/${this.state.name}`} >More info about {this.state.name} on Wikipedia</a>
-            <button onClick={this.axxios.bind(this)} class="btn btn-primary mt-5">New <span class="pokemon">Pokemon!</span></button>
-          </div>
-        </div>
-      <div>
-          
-          
-        </div>
-        
-      
+          name: res.data.forms[0].name,
+          pokemons: res.data.sprites,
+        });
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="container ">
+        <Card className="text-center cartadiv">
+          <Card.Header>Your unique Pokemon is...</Card.Header>
+          <Card.Body>
+            <Card.Title>
+              {this.state.name.charAt(0).toUpperCase() +
+                this.state.name.slice(1)}
+            </Card.Title>
+            <Card.Text>
+              <img
+                src={this.state.pokemons.front_default}
+                className="card-img-top-left imagen"
+                alt="img"
+              ></img>
+            </Card.Text>
+            <Button
+              variant="primary"
+              onClick={this.axxios.bind(this)}
+              className="btn btn-primary "
+            >
+              New <span className="pokemon">Pokemon!</span>
+            </Button>
+            <a
+              className="d-block mt-3"
+              href={`https://en.wikipedia.org/wiki/${this.state.name}`}
+            >
+              More info about {this.state.name} on Wikipedia
+            </a>
+          </Card.Body>
+          <Card.Footer className="text-muted">
+            Lucio Colombo @2020. Powered by PokeApi
+          </Card.Footer>
+        </Card>
       </div>
-      <footer >
-        <p>Pokecards! Lucio Colombo //  2020 Design // Powered by PokeApi</p>  
-      </footer>
-      </div>
-    )
+    );
   }
 }
-
-
